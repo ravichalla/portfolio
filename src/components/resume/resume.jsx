50ff9997dee37d82e824d3a653f0c01f937fb18c
Ravi Challa <ravichalla95@gmail.com>
2019-02-23 19:05:54 -0600

    Update resume.jsx

--- src/components/resume/resume.jsx
+++ src/components/resume/resume.jsx
@@ -1,201 +1,87 @@
 import React, { Component } from "react";
-export default class Header extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        {/* Resume Section
-   ================================================== */}
-        <section id="resume">
-          {/* Education
------------------------------------------------ */}
-          <div className="row education">
-            <div className="three columns header-col">
-              <h1>
-                <span>Education</span>
-              </h1>
+export default class About extends Component {
+render() {
+return (
+<React.Fragment>
+   <section id="about">
+      <div className="row">
+         <div className="three columns">
+            <img className="profile-pic" src="images/profilepic.jpg" alt />
+         </div>
+         <div className="nine columns main-col">
+            <h2>About Me</h2>
+            <p>
+               I am pursuing my Masters from University of Central Missouri in
+               Computer Science.I hold an Undergraduate degree from Andhra
+               University. I have taken up courses like Advanced
+               Algorithms,Advanced Computer Networks,Advanced Database
+               Systems,Compiler Design,Advanced Software Engineering,Cloud
+               Computing, Artificial Intelligence,Advanced Operating Systems.
+               During the coursework, I was involved in development and
+               implementation of various projects for the same courses which
+               gave me immense knowledge about different new technologies as
+               well as gave me exposure and confidence.
+            </p>
+            <p>
+               I worked as web developer in Wright State University, in Knoesis
+               department,where I had maintained the profiles of people, their
+               web portfolios in server and CRUD operations on website.Also
+               perfomed latest security patches for Drupal Content management
+               system on Linux server(Ubuntu).
+            </p>
+            <p>
+               I also have around 1.5 years of technical experience in
+               application and web development at Tata Consultancy Services for
+               Visteon. I have in-depth knowledge and professional experience
+               in all phases of the software development life cycle and agile
+               methodologies.My objective is to make a long term commitment to
+               work in a challenging opportunistic environment where I can
+               apply my knowledge as well as learn to enhance my technological,
+               leadership and communication skills.
+            </p>
+            <p>
+               Developed project related to java and sql as team member in multiple projects 
+               related to PaaS and multi domain areas of finance, ecommerce and electronic sector.
+               Trained and escalated to research and development team of Ezues R&D
+            </p>
+            <div className="row">
+               <div className="columns contact-details">
+                  <h2>Contact Details</h2>
+                  <p className="address">
+                     <span>Ravi Challa</span>
+                     <span>
+                     <br />
+                     USA
+                     </span>
+                     <br />
+                     <span>
+                     <a href="ravichalla.herokuapp.com">
+                     ravichalla.herokuapp.com
+                     </a>
+                     </span>
+                  </p>
+               </div>
+               <div className="columns download">
+                  <p>
+                     <a href="" className="button">
+                     <i className="fa fa-download" />
+                     Resume(Contact me for Updated Resume)
+                     </a>
+                  </p>
+               </div>
             </div>
-            <div className="nine columns main-col">
-              <div className="row item">
-                <div className="twelve columns">
-                  <h3>University of Central Missouri</h3>
-                  <p className="info">
-                    Masters in Computer Science <span>•</span>{" "}
-                    <em className="date">Jan 2019</em>
-                  </p>
-                  <p>
-                    Did different projects and learnt real computer sciene
-                    techniques indepth ,so to apply those in real world problem
-                    solving and for enterprises.see the portfolio.
-                  </p>
-                </div>
-              </div>{" "}
-              {/* item end */}
-              {/*
-              <div className="row item">
-                <div className="twelve columns">
-                  <h3>School of Cool Designers</h3>
-                  <p className="info">
-                    B.A. Degree in Graphic Design <span>•</span>{" "}
-                    <em className="date">March 2003</em>
-                  </p>
-                  <p>
-                    This is Photoshop's version of Lorem Ipsum. Proin gravida
-                    nibh vel velit auctor aliquet. Aenean sollicitudin, lorem
-                    quis bibendum auctor, nisi elit consequat ipsum, nec
-                    sagittis sem nibh id elit. Duis sed odio sit amet nibh
-                    vulputate cursus a sit amet mauris. Morbi accumsan ipsum
-                    velit. Nam nec tellus a odio tincidunt auctor a ornare odio.
-                    Sed non mauris vitae erat
-                  </p>
-                </div>
-</div>{" "}        */}
-              {/* item end */}
-            </div>{" "}
-            {/* main-col end */}
-          </div>{" "}
-          {/* End Education */}
-          {/* Work
------------------------------------------------ */}
-          <div className="row work">
-            <div className="three columns header-col">
-              <h1>
-                <span>Work</span>
-              </h1>
-            </div>
-            <div className="nine columns main-col">
-              <div className="row item">
-                <div className="twelve columns">
-                  <h3>University Of Central Missouri</h3>
-                  <p className="info">
-                    AI Lab Assistant and Technical Support<span>•</span>{" "}
-                    <em className="date">August 2018 - Present</em>
-                  </p>
-                  <p>
-                    Practical building of latest neural networks and ensemble style of algorithm.
-                    Currently working on an app,for tracking the location of shuttle bus, which has users of about 70 people.
-                    Responsible for maintaining the WIFI service by using Cisco-Meraki Web app. Manage facilities and equipment usage and provide technical support as needed.
-                    Coordinate the tasks , that happen during any events in the university. 
-                  </p>
-                </div>
-              </div>{" "}
-              {/* item end */}
-              <div className="row item">
-                <div className="twelve columns">
-                  <h3> Wright State University</h3>
-                  <h3 />
-                  <p className="info">
-                    Web Developer<span>•</span>{" "}
-                    <em className="date">Feb 2018 - Jun 2018</em>
-                  </p>
-                  <p>
-                    Responsible for developing, updating and managing the
-                    Knoesis website using Drupal CMS which has monthly 800
-                    unique users. • Responsible for making technical
-                    documentation for the website • Updating and Upgrading of
-                    Drupal site, whenever security update released by Drupal •
-                    Make use of Kanban Board, for taking the tasks .Used to
-                    attend daily Scrum meeting.
-                  </p>
-                </div>
-              </div>{" "}
-              {/* item end */}
-              <div className="row item">
-                <div className="twelve columns">
-                  <h3>Tata Consultancy Services</h3>
-                  <p className="info">
-                    Software Engineer <span>•</span>{" "}
-                    <em className="date">October 2016 - December 2017</em>
-                  </p>
-                  <p>
-                    Worked with different clients such as Visteon, Johnson
-                    Controls.Developed Applications and Software for the
-                    clients,for them.Have experience in Agile Scrum methodology
-                    and effective implementation of design patterns.The roles
-                    and responsiblities include requirement gathering and
-                    analysis,come up with business proposals and work
-                    estimates.Also designed and developed major modules of the
-                    project.Has been coordinated between with different teams
-                    for various integration purposes like Jcaps etc.Code
-                    analysis and performance optimization.Done enhancements and
-                    Change Requests.Gave POC demonstration to client.Deployment
-                    and configuration of web applications perfomed.
-                  </p>
-                </div>
-              </div>{" "}
-              {/* item end */}
-            </div>{" "}
-            <div className="row item">
-                <div className="twelve columns">
-                  <h3>Ezues Private Limited</h3>
-                  <p className="info">
-                    Junior Software Engineer<span>•</span>{" "}
-                    <em className="date">January 2016 - October 2017</em>
-                  </p>
-                  <p>
-                    Developed project related to java and sql as team member in multiple projects 
-                    related to PaaS and multi domain areas of finance, ecommerce and electronic sector. 
-                    Trained and escalated to research and development team of Ezues R&D
-                  </p>
-                </div>
-              </div>{" "}
-              {/* item end */}
-            {/* main-col end */}
-          </div>{" "}
-          {/* End Work */}
-          {/* Skills
------------------------------------------------ */}
-          <div className="row skill">
-            <div className="three columns header-col">
-              <h1>
-                <span>Skills</span>
-              </h1>
-            </div>
-            <div className="nine columns main-col">
-              <p>Some of my skills are:</p>
-              <div className="bars">
-                <ul className="skills">
-                  <li>
-                    <span className="bar" style={{ width: "80%" }} />
-                    <em>Java</em>
-                  </li>
-                  <li>
-                    <span className="bar" style={{ width: "70%" }} />
-                    <em>Python</em>
-                  </li>
-                  <li>
-                    <span className="bar" style={{ width: "70%" }} />
-                    <em>JavaScript(React JS,Node JS)</em>
-                  </li>
-                  <li>
-                    <span className="bar" style={{ width: "80%" }} />
-                    <em>Containers</em>
-                  </li>
-                  <li>
-                    <span className="bar" style={{ width: "80%" }} />
-                    <em>MicroServices</em>
-                  </li>
-                  <li>
-                    <span className="bar" style={{ width: "80%" }} />
-                    <em>CI/CD</em>
-                  </li>
-                  <li>
-                    <span className="bar" style={{ width: "80%" }} />
-                    <em>Kubernetes & Open Shift</em>
-                  </li>
-                  <li>
-                    <span className="bar" style={{ width: "80%" }} />
-                    <em>BPM/Rules</em>
-                  </li>
-                </ul>
-              </div>
-              {/* end bars */}
-            </div>{" "}
-            {/* main-col end */}
-          </div>{" "}
-          {/* End skills */}
-        </section>{" "}
-        {/* Resume Section End*/}
-      </React.Fragment>
-    );
-  }
+            {" "}
+            {/* end row */}
+         </div>
+         {" "}
+         {/* end .main-col */}
+      </div>
+   </section>
+   {" "}
+   {/* About Section End*/}
+   {/* Resume Section
+   */}
+</React.Fragment>
+);
+}
 }
